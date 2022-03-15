c4138e723e897e4f2e2a46fea7753ebbeeea3fab
ZuluSpl0it <55359362+ZuluSpl0it@users.noreply.github.com>
2022-03-14 19:59:18 -0600

    Update upnp.go

--- upnp.go
+++ upnp.go
@@ -33,9 +33,9 @@ import (
 	"strings"
 	"time"
 
-	"gitlab.com/NebulousLabs/fastrand"
-	"gitlab.com/NebulousLabs/go-upnp/goupnp"
-	"gitlab.com/NebulousLabs/go-upnp/goupnp/dcps/internetgateway1"
+	"github.com/zuluspl0it/fastrand"
+	"github.com/zuluspl0it/go-upnp/goupnp"
+	"github.com/zuluspl0it/go-upnp/goupnp/dcps/internetgateway1"
 )
 
 // An IGD provides an interface to the most commonly used functions of an
